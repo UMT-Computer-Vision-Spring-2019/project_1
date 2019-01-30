228411d6cdeeb1240b64f5ce92f2aa32e6c7df14
Unknown <mathewgutierrez00@gmail.com>
2019-01-29 17:59:12 -0700

    Work on Pose estimation

--- poseEstimation.py
+++ poseEstimation.py
@@ -4,96 +4,98 @@ import sys
 
 
 class Camera(object):
-    def __init__(self):
-        self.p = None                   # Pose
-        self.f = None                   # Focal Length in Pixels
-        self.sensorX = None
-        self.sensorY = None
-        self.x = None
-        self.y = None
-        self.c = np.array([None,None])  #
+    def __init__(self,p,f,c):
+        self.p = p                   # Pose
+        self.f = f                   # Focal Length in Pixels
+        self.c = c
 
-    def projective_transform(self, p0, p1, p2):
-        x = p0/p2
-        y = p1/p2
+    def projective_transform(self,X):
+        """
+        This function performs the projective transform on generalized coordinates in the camera reference frame.
+        """
+        projected = np.array([])
 
-        u = self.f * x + self.x
-        v = self.f * y + self.y
+        x = X[:,0]/X[:,2]
+        y = X[:,1]/X[:,2]
 
-        return [u,v]
+        u = self.f * x + self.c[0]/2
+        v = self.f * y + self.c[1]/2
 
-    def rotational_transform(self, x):
-
-        cosAz= np.cos(self.p[3])
-        sinAz= np.sin(self.p[3])
-
-        cosPch= np.cos(self.p[4])
-        sinPch= np.sin(self.p[4])
-
-        cosRoll= np.cos(self.p[5])
-        sinRoll= np.sin(self.p[5])
+        u = np.hstack(u)
+        v = np.hstack(v)
+        return u,v
 
 
-        T = [
-        [1, 0, 0,-self.p[0]],
-        [0, 1, 0,-self.p[1]],
-        [0, 0, 1,-self.p[2]],
-        [0, 0, 0, 1]
-        ]
+    def rotational_transform(self, X, p):
 
-        Ryaw = [
+        """
+        This function performs the translation and rotation from world coordinates into generalized camera coordinates.
+        """
+
+
+        cosAz= np.cos(p[3])
+        sinAz= np.sin(p[3])
+
+        cosPch= np.cos(p[4])
+        sinPch= np.sin(p[4])
+
+        cosRoll= np.cos(p[5])
+        sinRoll= np.sin(p[5])
+
+
+        T = np.mat([
+        [1, 0, 0,-p[0]],
+        [0, 1, 0,-p[1]],
+        [0, 0, 1,-p[2]],
+        [0, 0, 0, 1]])
+
+        Ryaw = np.mat([
         [cosAz, -sinAz, 0, 0],
         [sinAz, cosAz, 0, 0],
-        [0, 0, 1, 0]
-        ]
+        [0, 0, 1, 0]])
 
-        Rpitch = [
+        Rpitch = np.mat([
         [1, 0, 0],
         [0, cosPch, sinPch],
-        [0, -sinPch, cosPch]
-        ]
+        [0, -sinPch, cosPch]])
 
-        Rroll = [
+        Rroll = np.mat([
         [cosRoll, 0, -sinRoll],
         [0, 1, 0 ],
-        [sinRoll, 0 , cosRoll]
-        ]
+        [sinRoll, 0 , cosRoll]])
 
-        Raxis = [
+        Raxis = np.mat([
         [1, 0, 0],
         [0, 0, -1],
-        [0, 1 , 0]
-        ]
-        point = [x[0],x[1],x[2],1]
+        [0, 1 , 0]])
 
-        translated = np.matmul(T,point)
-        yawed = np.matmul(Ryaw,translated)
-        pitched = np.matmul(Rpitch,yawed)
-        rolled = np.matmul(Rroll,pitched)
-        swapped = np.matmul(Raxis,rolled)
+        C = Raxis @ Rroll @ Rpitch @ Ryaw @ T
 
-        return self.projective_transform(swapped[0],swapped[1],swapped[2])
+        X = X.dot(C.T)
 
+        u,v = self.projective_transform(X)
+
+        return u,v
     def estimate_pose(self,X_gcp,u_gcp):
-
-        for gcp in X_gcp:
-            gcp = self.rotational_transform(gcp)
-
-        p_opt = ls(self.residual, self.p, method='lm',args=(X_gcp,u_gcp))['x']
         """
         This function adjusts the pose vector such that the difference between the observed pixel coordinates u_gcp
         and the projected pixels coordinates of X_gcp is minimized.
         """
-    def residual(self,p,X,U):
-        self.p = p
-        resid =[]
-        for i,gcp in enumerate(X):
-            pred = self.rotational_transform(gcp)
-            resid.append(pred[0] - U[i][0])
-            resid.append(pred[1] - U[i][1])
 
+        p_opt = ls(self.residual, self.p, method='lm',args=(X_gcp,u_gcp))['x']
+
+        self.p = p_opt
+        return p_opt
+    def residual(self,p,X,u_gcp):
+
+        u,v = self.rotational_transform(X,p)
+
+        u = np.squeeze(np.asarray(u - u_gcp[:,0]))
+        v = np.squeeze(np.asarray(v - u_gcp[:,1]))
+        resid = np.stack((u, v), axis=-1)
+        resid = resid.flatten()
+        #print(resid)
         return resid
-
 
 
 
@@ -102,41 +104,30 @@ def main(argv):
     FOCAL_LENGTH = 2448
     SENSOR_X = 3264
     SENSOR_Y = 2448
-    cam = Camera()
-    cam.f = FOCAL_LENGTH
-    cam.sensorX = SENSOR_X
-    cam.sensorY = SENSOR_Y
-    cam.x = cam.sensorX/2
-    cam.y = cam.sensorY/2
-    cam.p = np.array([0,0,0,0,0,0])
 
-    data = [
-    "|1984|1053|272558.68|5193938.07|1015 |Main hall spire|",
+    f = FOCAL_LENGTH
+    c = np.array([SENSOR_X,SENSOR_Y])
+    p = np.array([0,0,0,0,0,0])
+    cam = Camera(p,f,c)
+    p_0 = np.array([0,0,0,0,0,0])
+    obs = np.array([[272558.68, 5193938.07, 1015.,1],
+              [272572.34, 5193981.03, 982.,1],
+              [273171.31, 5193846.77, 1182.,1],
+              [273183.35, 5194045.24, 1137.,1],
+              [272556.74, 5193922.02, 998.,1]])
 
-    "|884 |1854|272572.34|5193981.03|982 |Large spruce|",
+    true = np.array([[1984., 1053.],
+                      [884., 1854.],
+                      [1202., 1087.],
+                      [385., 1190.],
+                      [2350., 1442.]])
 
-    "|1202|1087|273171.31|5193846.77|1182 |Bottom of left tine of M|",
 
-    "|385 |1190|273183.35|5194045.24|1137 |Large rock outcrop on Sentinel|",
 
-    "|2350|1442|272556.74|5193922.02|998 |Southernmost window apex on main hall|"
-    ]
-
-    obs = []
-    true = []
-
-    for GCP in data:
-        GCP = GCP.strip("|")
-        GCP = GCP.split("|")
-        true.append([float(GCP[0]),float(GCP[1])])
-        obs.append([float(GCP[2]),float(GCP[3]),float(GCP[4])])
-
-    cam.estimate_pose(obs,true)
-    
-    print(cam.p)
+    p_opt = cam.estimate_pose(obs,true)
 
     for GCP in obs:
-        print(cam.rotational_transform(GCP))
+        print(cam.rotational_transform(GCP,p_opt))
 
 
 if __name__=='__main__':
