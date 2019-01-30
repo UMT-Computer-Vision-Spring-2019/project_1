b7932b775ba264cd8fbc79e27cb9fc1e8f649311
Thomas Colligan <thomas.colligan@outlook.com>
2019-01-30 09:03:26 -0700

    Working solution to project and bonus

--- annotate.py
+++ annotate.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-im = plt.imread('IMG_0433.JPG')
+ims = ['campus_stereo_1.jpg', 'campus_stereo_2.jpg']
 
-plt.figure(figsize=(12, 8))
-
-plt.imshow(im)
-coords = plt.ginput(n=1, show_clicks=True)
-print(coords)
+for f in ims:
+    im = plt.imread(f)
+    plt.figure(figsize=(12, 8))
+    plt.imshow(im)
+    coords = plt.ginput(n=1, show_clicks=True)
+    print(coords)
 
